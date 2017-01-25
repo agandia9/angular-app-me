03be5b3eb65e0713bb520c0dc5c40b765d53f886
alejandro <alejandro.gandia@gmail.com>
2017-01-25 16:36:05 +0100

    first commit

---
+++ js/MainController.js
@@ -0,0 +1,9 @@
+var app = angular.module("myApp");
+
+app.controller('MainController', function($scope){
+  $scope.title = 'Top Alejan';
+  $scope.promo = 'Something promo...';
+  $scope.favoriteGame;
+  $scope.favoriteSuperhero;
+  $scope.lenguages;
+});
---
+++ js/app.js
@@ -0,0 +1,5 @@
+// So far this is our typical workflow when making an AngularJS app:
+// Create a module, and use ng-app in the view to define the application scope.
+// Create a controller, and use ng-controller in the view to define the controller scope.
+// Add data to $scope in the controller so they can be displayed with expressions in the view.
+angular.module("myApp", []);
