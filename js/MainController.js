5cad48571f3cc270f1ce04301ed4d2c058f2c738
alejandro <alejandro.gandia@gmail.com>
2017-01-26 15:03:02 +0100

    repeat ex. ok

--- js/MainController.js
+++ js/MainController.js
@@ -1,12 +1,15 @@
 var app = angular.module("myApp");
 
 app.controller('MainController', function($scope) {
+	//$scope.modals
 	$scope.title = "Hello ma'lady...";
 	$scope.promo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est quisquam unde aperiam reiciendis quos harum consequuntur dolorem delectus velit minima, tempore odit blanditiis facilis voluptas, expedita quo laboriosam cupiditate fugit!';
 	$scope.favoriteGame;
 	$scope.favoriteSuperhero;
 	$scope.value = 1;
 	$scope.funky = ":(";
+
+	//$scope.functions
 	$scope.isBold = function() {
 		return $scope.value % 2 === 0; };
 	$scope.isItalic = function() {
@@ -18,4 +21,14 @@ app.controller('MainController', function($scope) {
 			$scope.funky = "YEA";
 		}
 		return $scope.value % 10 === 0; };
+	//$scope.repeat
+	$scope.countries = [
+			{id: 1, name: "Kazakhstan", population:3054015, cont:"umbrae"},
+			{id: 2, name: "Sri Lanka", population:46889, cont:"cardiospasm"},
+			{id: 3, name: "Pakistan", population:764411, cont:"agonizedly"},
+			{id: 4, name: "Estonia", population:9654801, cont:"asthma"},
+			{id: 5, name: "Tajikistan", population:87, cont:"fumigatory"},
+			{id: 6, name: "Denmark", population:23854, cont:"undraped"},
+			{id: 7, name: "Philippines", population:626190, cont:"hagborn"}
+		];
 });
