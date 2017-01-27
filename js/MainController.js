9407355efe6523ce7ee8c9205f151ba8c6d8bc30
alejandro <alejandro.gandia@gmail.com>
2017-01-27 10:13:15 +0100

    ng-value ok

--- js/MainController.js
+++ js/MainController.js
@@ -1,34 +1,51 @@
 var app = angular.module("myApp");
+app.value('myName', "Alex");
+app.controller('MainController',["$scope", "myName", function($scope, myName) {
+    //$scope.modals
+    $scope.title = "Hello ma'lady...";
+    $scope.promo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est quisquam unde aperiam reiciendis quos harum consequuntur dolorem delectus velit minima, tempore odit blanditiis facilis voluptas, expedita quo laboriosam cupiditate fugit!';
+    $scope.favoriteGame;
+    $scope.favoriteSuperhero;
+    $scope.value = 1;
+    $scope.funky = ":(";
+    $scope.myName = myName;
 
-app.controller('MainController', function($scope) {
-	//$scope.modals
-	$scope.title = "Hello ma'lady...";
-	$scope.promo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est quisquam unde aperiam reiciendis quos harum consequuntur dolorem delectus velit minima, tempore odit blanditiis facilis voluptas, expedita quo laboriosam cupiditate fugit!';
-	$scope.favoriteGame;
-	$scope.favoriteSuperhero;
-	$scope.value = 1;
-	$scope.funky = ":(";
+    //$scope.functions
+    $scope.isBold = function() {
+        return $scope.value % 2 === 0;
+    };
+    $scope.isItalic = function() {
+        return $scope.value % 3 === 0;
+    };
+    $scope.isUnderlined = function() {
+        return $scope.value % 5 === 0;
+    };
+    $scope.isFunky = function() {
+        if ($scope.value % 10 === 0) {
+            $scope.funky = "YEA";
+        }
+        return $scope.value % 10 === 0;
+    };
+    //$scope.repeat
+    $scope.countries = [
+        { id: 1, name: "Kazakhstan", population: 3054015, cont: "umbrae" },
+        { id: 2, name: "Sri Lanka", population: 46889, cont: "cardiospasm" },
+        { id: 3, name: "Pakistan", population: 764411, cont: "agonizedly" },
+        { id: 4, name: "Estonia", population: 9654801, cont: "asthma" },
+        { id: 5, name: "Tajikistan", population: 87, cont: "fumigatory" },
+        { id: 6, name: "Denmark", population: 23854, cont: "undraped" },
+        { id: 7, name: "Philippines", population: 626190, cont: "hagborn" }
+    ];
+}]);
+app.controller('secondController', function($scope) {
+    $scope.isFirstElementVisible = true;
+    $scope.isSecondElementVisible = true;
 
-	//$scope.functions
-	$scope.isBold = function() {
-		return $scope.value % 2 === 0; };
-	$scope.isItalic = function() {
-		return $scope.value % 3 === 0; };
-	$scope.isUnderlined = function() {
-		return $scope.value % 5 === 0; };
-	$scope.isFunky = function() {
-		if($scope.value % 10 === 0){
-			$scope.funky = "YEA";
-		}
-		return $scope.value % 10 === 0; };
-	//$scope.repeat
-	$scope.countries = [
-			{id: 1, name: "Kazakhstan", population:3054015, cont:"umbrae"},
-			{id: 2, name: "Sri Lanka", population:46889, cont:"cardiospasm"},
-			{id: 3, name: "Pakistan", population:764411, cont:"agonizedly"},
-			{id: 4, name: "Estonia", population:9654801, cont:"asthma"},
-			{id: 5, name: "Tajikistan", population:87, cont:"fumigatory"},
-			{id: 6, name: "Denmark", population:23854, cont:"undraped"},
-			{id: 7, name: "Philippines", population:626190, cont:"hagborn"}
-		];
 });
+
+
+// angular.module("root", [])
+// .value("message", "Hello World")
+// .controller("index", ["$scope", "message", function($scope, message){
+//     $scope.message = message;
+// }])
